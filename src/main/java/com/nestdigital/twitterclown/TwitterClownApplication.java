4a23f0f1cfa360c071f7470438443105f0f53040
kishore951 <kishoresakthivel951@gmail.com>
2022-10-21 12:52:12 +0530

    new

---
+++ src/main/java/com/nestdigital/twitterclown/TwitterClownApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.twitterclown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterClownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterClownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/twitterclown/TwitterClownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.twitterclown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterClownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
